eeb03705ffee817e6941f66adfad161bec0d8742
JesseStan <stany2704@gmail.com>
2026-02-02 01:01:51 +0400

    l

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.ai_image import gemini
+from backend.ai_bots import gemini
 from pydantic import BaseModel
 
 app = FastAPI()
